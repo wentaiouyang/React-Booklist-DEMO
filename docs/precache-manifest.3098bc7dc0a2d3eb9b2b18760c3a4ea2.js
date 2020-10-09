6420e777d0f122a12e490e702604e1f6af176cd9
wentaiouyang <wentaiouyang@gmail.com>
2020-10-09 22:23:29 +1000

    new update

---
+++ docs/precache-manifest.3098bc7dc0a2d3eb9b2b18760c3a4ea2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439dadb37ce58ac8f9dd25398b5f3fce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ea99419c1382a15b315",
+    "url": "/static/css/2.25d8efc1.chunk.css"
+  },
+  {
+    "revision": "33993665d043b9937475",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8ea99419c1382a15b315",
+    "url": "/static/js/2.3f5f718e.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.3f5f718e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33993665d043b9937475",
+    "url": "/static/js/main.9b17b8a4.chunk.js"
+  },
+  {
+    "revision": "05d45e40dc7c7ad03e1a",
+    "url": "/static/js/runtime-main.62c2c010.js"
+  }
+]);
